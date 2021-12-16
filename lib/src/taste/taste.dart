38fb3a80aca3d193056fe3d43d80b6564d9c6b83
avataromvatar <avataromvatar@yandex.ru>
2021-12-16 23:34:41 +0400

    save

--- lib/src/taste/taste.dart
+++ lib/src/taste/taste.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
 
+import 'package:tastypie/src/layer/layer.dart';
 import 'package:tastypie/src/taste/taste_dto.dart';
 
 enum ITasteType { INPUT, OUTPUT }
 
 abstract class ITaste {
   ITasteType get type;
+  bool get onlyInTheLayer;
   String get topic;
   int get stateMask;
 }
@@ -16,6 +18,7 @@ abstract class ITasteOutput implements ITaste {
   void connect(List<ITasteInput> inputs);
   void disconnect(List<ITasteInput> inputs);
   void disconnectAll();
+  bool send(ITasteDTO dto);
 }
 
 abstract class ITasteInput implements ITaste {
@@ -25,4 +28,85 @@ abstract class ITasteInput implements ITaste {
   set handler(
       void Function(dynamic data, String topic, int state, ITasteOutput? output)
           fun);
+  bool call(ITasteDTO dto);
+}
+
+class TasteCommon implements ITaste {
+  final ITasteType type;
+  final String topic;
+  final int stateMask;
+  final bool onlyInTheLayer;
+  TasteCommon(this.type, this.topic,
+      {this.stateMask = 0xFFFFFFFF, this.onlyInTheLayer = false});
+}
+
+class TasteInput extends TasteCommon implements ITasteInput {
+  StreamController<ITasteDTO> _streamController = StreamController<ITasteDTO>();
+  ITasteOutput? _outputLink;
+  void Function(dynamic data, String topic, int state, ITasteOutput? output)
+      _handler;
+  TasteInput(String topic, this._handler,
+      {int stateMask = 0xFFFFFFFF, ITasteOutput? outputLink})
+      : _outputLink = outputLink,
+        super(ITasteType.INPUT, topic, stateMask: stateMask);
+
+  StreamSink<ITasteDTO> get inputStream => _streamController.sink;
+  ITasteOutput? get outputTaste => _outputLink;
+  set link(ITasteOutput output) => _outputLink = output;
+  set handler(
+          void Function(
+                  dynamic data, String topic, int state, ITasteOutput? output)
+              fun) =>
+      _handler = fun;
+  bool call(ITasteDTO dto) {
+    if (dto.topic == topic && (stateMask & dto.state != 0)) {
+      _handler(dto.data, topic, dto.state, _outputLink);
+      return true;
+    }
+    return false;
+  }
+}
+
+class TasteOutput extends TasteCommon implements ITasteOutput {
+  final StreamController<ITasteDTO> _streamController =
+      StreamController<ITasteDTO>();
+  final List<ITasteInput> _links = List<ITasteInput>.empty(growable: true);
+
+  TasteOutput(String topic,
+      {int stateMask = 0xFFFFFFFF, ITasteOutput? outputLink})
+      : super(ITasteType.OUTPUT, topic, stateMask: stateMask);
+
+  Stream<ITasteDTO> get outputStream => _streamController.stream;
+  List<ITasteInput> get connections => _links;
+
+  bool send(ITasteDTO dto) {
+    bool isSended = false;
+    if (dto.topic == topic && (stateMask & dto.state != 0)) {
+      connections.forEach((element) {
+        element.call(dto);
+        isSended = true;
+      });
+    }
+    return isSended;
+  }
+
+  void connect(List<ITasteInput> inputs) {
+    inputs.forEach((element) {
+      if (!_links.contains(element)) {
+        _links.add(element);
+      }
+    });
+  }
+
+  void disconnect(List<ITasteInput> inputs) {
+    inputs.forEach((element) {
+      if (_links.contains(element)) {
+        _links.remove(element);
+      }
+    });
+  }
+
+  void disconnectAll() {
+    _links.clear();
+  }
 }
--- lib/src/topping/topping.dart
+++ lib/src/topping/topping.dart
@@ -7,18 +7,80 @@ abstract class ITopping {
       String topic,
       void Function(dynamic data, String topic, int state, ITasteOutput? output)
           handler,
-      {int state = 0xFFFFFFFF,
+      {int stateMask = 0xFFFFFFFF,
       ITasteOutput? link});
   bool addOutputTaste(String topic,
-      {List<ITasteInput>? connections, int state = 0xFFFFFFFF});
+      {List<ITasteInput>? connections, int stateMask = 0xFFFFFFFF});
   bool addTaste(ITaste taste);
+
+  ///send dto if filter outputTaste check ok.
   bool send(ITasteDTO dto);
+  List<ITaste> getTaste(String topic, {int stateMask = 0xFFFFFFFF});
 }
 
 abstract class IToppingMechanics implements ITopping {
   ///Then Toping add to TastyPieLayer? TastyPieLayer set self to [layer]
-  ITastyPieLayer? get layer;
-  set layer(ITastyPieLayer? layer);
+  ITastyPieLayer? get own_layer;
+  set own_layer(ITastyPieLayer? layer);
+
+  ///Send from [taste] to other layer or extern
+  void sendFromTaste(ITasteDTO dto, ITasteOutput taste);
+
+  ///TastyPieLayer call then get message
+  void call(ITasteDTO dto);
+
+  ///TastyPieLayer call when this topping remove from layer
+  void complete();
+}
+
+class Topping implements IToppingMechanics {
+  List<ITasteInput> _input_taste = List<ITasteInput>.empty(growable: true);
+  List<ITasteOutput> _output_taste = List<ITasteOutput>.empty(growable: true);
+
+  bool addInputTaste(
+      String topic,
+      void Function(dynamic data, String topic, int state, ITasteOutput? output)
+          handler,
+      {int stateMask = 0xFFFFFFFF,
+      ITasteOutput? link}) {
+    //TODO analize _input_taste before add
+    _input_taste.add(
+        TasteInput(topic, handler, stateMask: stateMask, outputLink: link));
+    return true;
+  }
+
+  bool addOutputTaste(String topic,
+      {List<ITasteInput>? connections, int stateMask = 0xFFFFFFFF}) {
+    //TODO analize _input_taste before add
+    _output_taste.add(TasteOutput(topic, stateMask: stateMask));
+    return true;
+  }
+
+  bool addTaste(ITaste taste) {
+    if (taste is ITasteInput) {
+      _input_taste.add(taste);
+    } else if (taste is ITasteOutput) {
+      _output_taste.add(taste);
+    }
+    return true;
+  }
+
+  void sendFromTaste(ITasteDTO dto, ITasteOutput taste) {
+    //TODO
+  }
+  bool send(ITasteDTO dto) {
+    bool isSending = false;
+    _output_taste.forEach((element) {
+      if (element.send(dto)) isSending = true;
+    });
+    return isSending;
+  }
+
+  List<ITaste> getTaste(String topic, {int stateMask = 0xFFFFFFFF}) {}
+
+  ITastyPieLayer? _own_layer;
+  ITastyPieLayer? get own_layer => _own_layer;
+  set own_layer(ITastyPieLayer? layer) => _own_layer = layer;
 
   ///TastyPieLayer call then get message
   void call(ITasteDTO dto);
